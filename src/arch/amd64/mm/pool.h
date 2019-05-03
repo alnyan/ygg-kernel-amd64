e565006abcc5a1c3bda52fef1a76ba533deb716a
Mark <alnyan@airmail.cc>
2019-05-03 13:52:46 +0300

    [amd64] pool alloc/free clarify

--- src/arch/amd64/mm/pool.h
+++ src/arch/amd64/mm/pool.h
@@ -8,8 +8,8 @@
 /// Initialize the paging structure allocation pool
 void amd64_mm_pool_init(uintptr_t base, size_t size);
 
-/// Allocate a paging structure
+/// Allocate a paging structure (returns virtual addresses)
 uint64_t *amd64_mm_pool_alloc(void);
 
-/// Free a paging structure
+/// Free a paging structure (obj is a virtual pointer)
 void amd64_mm_pool_free(uint64_t *obj);
