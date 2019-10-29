5a3b8621bcde099d65738fadc758a6a5105843f4
Mark <alnyan@airmail.cc>
2019-10-29 10:26:11 +0200

    Added libc crtX.o

--- usr/init.c
+++ usr/init.c
@@ -1,10 +1,7 @@
 #include <unistd.h>
 #include <stdint.h>
 
-typedef uint64_t size_t;
-typedef int64_t ssize_t;
-
-void _start(void) {
+int main(int argc, char **argv) {
     char c;
 
     while (1) {
