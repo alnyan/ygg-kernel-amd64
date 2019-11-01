1a6fb5df178528a88cf6be8ac84f7696c9aef665
Mark <alnyan@airmail.cc>
2019-11-01 10:48:10 +0200

    Add SCI handler wrapper

--- sys/amd64/acpi_osl_irq.c
+++ sys/amd64/acpi_osl_irq.c
@@ -1,8 +1,18 @@
 #include "acpi.h"
+#include "sys/amd64/hw/irq.h"
 #include "sys/debug.h"
+
+static ACPI_OSD_HANDLER sci_wrapped;
+
+uint32_t sci_wrapper(void *ctx) {
+    kdebug("!!! SCI WRAPPER !!!\n");
+    return sci_wrapped(ctx);
+}
 
 ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 InterruptNumber, ACPI_OSD_HANDLER ServiceRoutine, void *Context) {
     kwarn("ACPI ISR INSTALL STUB\n");
+    sci_wrapped = ServiceRoutine;
+    irq_add_leg_handler(InterruptNumber, sci_wrapper, Context);
     return AE_OK;
 }
 
