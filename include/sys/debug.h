4584b22585a67dc1b38d53afef481259c9484886
Mark <alnyan@airmail.cc>
2020-01-17 12:15:23 +0200

    Add timer value in debug console output

--- include/sys/debug.h
+++ include/sys/debug.h
@@ -7,6 +7,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include "sys/time.h"
+
 #define DEBUG_DEFAULT   0
 #define DEBUG_INFO      1
 #define DEBUG_WARN      2
@@ -16,12 +18,15 @@
 #define DEBUG_OUT_SERIAL    0
 #define DEBUG_OUT_DISP      1
 
-#define kdebug(f, ...)      debugf(DEBUG_DEFAULT,   "[%s] " f, __func__, ##__VA_ARGS__)
-#define kinfo(f, ...)       debugf(DEBUG_INFO,      "[%s] " f, __func__, ##__VA_ARGS__)
-#define kwarn(f, ...)       debugf(DEBUG_WARN,      "\033[1m\033[33m[%s] " f "\033[0m", __func__, ##__VA_ARGS__)
-#define kerror(f, ...)      debugf(DEBUG_ERROR,     "\033[1m\033[31m[%s] " f "\033[0m", __func__, ##__VA_ARGS__)
-#define kfatal(f, ...)      debugf(DEBUG_FATAL,     "\033[41m[%s] " f "\033[0m", __func__, ##__VA_ARGS__)
-#define kprint(l, f, ...)   debugf(l,               "[%s] " f, __func__, ##__VA_ARGS__)
+#define DEBUG_BASE_FMT      "[%08lu %s] "
+#define DEBUG_BASE_ARGS     system_time / 1000000ULL, __func__
+
+#define kdebug(f, ...)      debugf(DEBUG_DEFAULT,   DEBUG_BASE_FMT f, DEBUG_BASE_ARGS, ##__VA_ARGS__)
+#define kinfo(f, ...)       debugf(DEBUG_INFO,      DEBUG_BASE_FMT f, DEBUG_BASE_ARGS, ##__VA_ARGS__)
+#define kwarn(f, ...)       debugf(DEBUG_WARN,      "\033[1m\033[33m" DEBUG_BASE_FMT f "\033[0m", DEBUG_BASE_ARGS, ##__VA_ARGS__)
+#define kerror(f, ...)      debugf(DEBUG_ERROR,     "\033[1m\033[31m" DEBUG_BASE_FMT f "\033[0m", DEBUG_BASE_ARGS, ##__VA_ARGS__)
+#define kfatal(f, ...)      debugf(DEBUG_FATAL,     "\033[41m" DEBUG_BASE_FMT f "\033[0m", DEBUG_BASE_ARGS, ##__VA_ARGS__)
+#define kprint(l, f, ...)   debugf(l,               DEBUG_BASE_FMT f, DEBUG_BASE_ARGS, ##__VA_ARGS__)
 
 void fmtsiz(char *buf, size_t sz);
 
