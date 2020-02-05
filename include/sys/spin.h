fc837b97d8c22ce7f538ad99a695aabdbc5abdeb
Mark <alnyan@airmail.cc>
2020-02-05 10:39:00 +0200

    Only use spinlocks in SMP scenario

--- include/sys/spin.h
+++ include/sys/spin.h
@@ -5,7 +5,14 @@
 #include "arch/amd64/sys/spin.h"
 #endif
 
+#if defined(AMD64_SMP)
 void spin_lock(spin_t *s);
 void spin_release(spin_t *s);
 void spin_lock_irqsave(spin_t *s, uintptr_t *irq);
 void spin_release_irqrestore(spin_t *s, uintptr_t *irq);
+#else
+#define spin_lock(s)
+#define spin_release(s)
+#define spin_lock_irqsave(s, i)
+#define spin_release_irqrestore(s, i)
+#endif
