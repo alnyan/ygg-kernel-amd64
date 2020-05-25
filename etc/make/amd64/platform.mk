dcf517f9cda8d701c37925c194e84aee71483b7c
Mark <alnyan@airmail.cc>
2020-05-25 12:38:21 +0300

    Add stub _entry64 for multiboot2

--- etc/make/amd64/platform.mk
+++ etc/make/amd64/platform.mk
@@ -8,7 +8,8 @@ ACPICA_OBJS=$(ACPICA_SRCS:%.c=$(O)/%.o)
 ACPICA_SRCD=$(shell find arch/amd64/acpica -type d)
 ACPICA_OBJD=$(ACPICA_SRCD:%=$(O)/%)
 
-OBJS+=$(ACPICA_OBJS) \
+OBJS+=$(O)/arch/amd64/entry64.o \
+	  $(ACPICA_OBJS) \
 	  $(O)/arch/amd64/acpi_osl_mem.o \
 	  $(O)/arch/amd64/acpi_osl_printf.o \
 	  $(O)/arch/amd64/acpi_osl_thread.o \
