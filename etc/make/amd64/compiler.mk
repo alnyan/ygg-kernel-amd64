836e2b3a4bdd16e14b5a95bbabdc46ccd69ae995
Mark <alnyan@airmail.cc>
2020-05-26 22:26:46 +0300

    Remove 32-bit dependency from makefiles

--- etc/make/amd64/compiler.mk
+++ etc/make/amd64/compiler.mk
@@ -1,19 +1,7 @@
-
-CC86?=i686-elf-gcc
-AR86?=i686-elf-ar
-LD86?=i686-elf-ld
-
 CC64?=x86_64-elf-gcc
 AR64?=x86_64-elf-ar
 LD64?=x86_64-elf-ld
 
-# Check that the tools exist
-ifeq (,$(shell which $(CC86)))
-$(error Failed to find "$${CC86}": $(CC86))
-endif
-ifeq (,$(shell which $(LD86)))
-	$(error Failed to find "$${LD86}": $(LD86))
-endif
 ifeq (,$(shell which $(CC64)))
 $(error Failed to find "$${CC64}": $(CC64))
 endif
