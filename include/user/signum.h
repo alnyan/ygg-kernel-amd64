954d5838ed2b4c7dc8a751d7fa47530d54bec0d7
Mark <alnyan@airmail.cc>
2020-05-29 18:00:00 +0300

    Add signal numbers

--- include/user/signum.h
+++ include/user/signum.h
@@ -1,11 +1,23 @@
 #pragma once
 
+#define SIGHUP      1
 #define SIGINT      2
+#define SIGQUIT     3
+#define SIGTRAP     5
 #define SIGABRT     6
 #define SIGKILL     9
 #define SIGSEGV     11
 #define SIGSYS      12
+#define SIGPIPE     13
+#define SIGALRM     14
 #define SIGTERM     15
-#define SIGCHLD     17
 #define SIGUSR1     30
 #define SIGUSR2     31
+
+#define SIGTSTP     18
+#define SIGCONT     19
+#define SIGCHLD     20
+#define SIGTTIN     21
+#define SIGTTOU     22
+
+#define NSIG        32
